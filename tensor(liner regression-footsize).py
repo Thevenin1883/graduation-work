2783176eba46ff47a712e1e4e567d8bca036e320
Thevenin1849 <sysy1849@gmail.com>
2025-01-09 16:59:12 +0900

    days2(last)

--- tensor(liner regression-footsize).py
+++ tensor(liner regression-footsize).py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 
-height = [170, 180, 175, 160]
-size = [260, 270, 265, 255]
-
+height = 170
+size = 260
+# size = height * a + b 선형 1차 모델
 a = tf.Variable(0.1)
 b = tf.Variable(0.2)
 
 def loss_func():
-    return tf.square(260 - (height * a + b))
+    return tf.square(260 - (height * a + b)) #실측값 - 예측값
 
-opt = tf.keras.optimizers.Adam(learning_rate=0.1)
-opt.minimize(loss_func,var_list = [a,b])
+opt = tf.keras.optimizers.Adam(learning_rate=0.1) # 경사 하강법 
+
+for i in range(300):
+    opt.minimize(loss_func,var_list = [a,b])
+    print(a.numpy(),b.numpy())
